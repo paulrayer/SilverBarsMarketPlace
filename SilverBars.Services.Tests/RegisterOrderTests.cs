a8b2a2b7bebd6a89b1f2447b3ee2018270eacb51
paulrayer <paul.rayer@hotmail.com>
2015-11-11 07:27:49 +0000

    Initial version with one failing unit test

---
+++ SilverBars.Services.Tests/RegisterOrderTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace SilverBars.Services.Tests
+{
+    [TestFixture]
+    public class RegisterOrderTests
+    {
+        private IOrderService _orderService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _orderService = new OrderService();
+        }
+
+        public RegisterOrderTests(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [Test]
+        public void RegisterOrderShouldAddAnOrderToTheCollection()
+        {
+           _orderService.RegisterOrder(new Order());
+            Assert.AreEqual(1, _orderService.Orders.Count);
+        }
+
+      
+    }
+
+    public class OrderService : IOrderService
+    {
+        public List<Order> Orders { get; set; }
+        
+        public void RegisterOrder(Order order)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Order
+    {
+    }
+
+    public interface IOrderService
+    {
+        List<Order> Orders { get; set; }
+        void RegisterOrder(Order order);
+    }
+}
